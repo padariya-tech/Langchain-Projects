65b846453cb808ef9f33f992bd6ff977f7dbf6a8
Nij Padariya <nijpadariya@Nijs-MacBook-Air.local>
2026-06-27 10:59:33 +0530

    ChatModel Implementation

---
+++ ChatModels/chat_model_demo.py
@@ -0,0 +1,11 @@
+from langchain_openai import ChatOpenAI
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatOpenAI(model='gpt-4.1')
+
+result = model.invoke("what is the capital of india")
+
+print(result)
